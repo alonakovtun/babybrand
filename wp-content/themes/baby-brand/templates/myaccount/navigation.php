7eeee4b127af65d67fe2451593bec61f78463f60
alonakovtun <alonakovtun55@gmail.com>
2022-04-01 20:20:00 +0200

    account menu #2

--- wp-content/themes/baby-brand/templates/myaccount/navigation.php
+++ wp-content/themes/baby-brand/templates/myaccount/navigation.php
@@ -22,14 +22,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 do_action( 'woocommerce_before_account_navigation' );
 ?>
 
-<nav class="woocommerce-MyAccount-navigation">
-	<ul>
-		<?php foreach ( wc_get_account_menu_items() as $endpoint => $label ) : ?>
-			<li class="<?php echo wc_get_account_menu_item_classes( $endpoint ); ?>">
-				<a href="<?php echo esc_url( wc_get_account_endpoint_url( $endpoint ) ); ?>"><?php echo esc_html( $label ); ?></a>
-			</li>
-		<?php endforeach; ?>
-	</ul>
+<nav class="woocommerce-MyAccount-navigation accountdetails">
+	<div class="accountdetails__container container">
+		<div class="accountdetails__body">
+			<ul class="adress__list">
+				<?php foreach (wc_get_account_menu_items() as $endpoint => $label) : ?>
+					<li class="<?php echo wc_get_account_menu_item_classes($endpoint); ?>">
+						<a class="adress__link" href="<?php echo esc_url(wc_get_account_endpoint_url($endpoint)); ?>"><?php echo esc_html($label); ?></a>
+					</li>
+				<?php endforeach; ?>
+			</ul>
+		</div>
+	</div>
 </nav>
 
 <?php do_action( 'woocommerce_after_account_navigation' ); ?>
