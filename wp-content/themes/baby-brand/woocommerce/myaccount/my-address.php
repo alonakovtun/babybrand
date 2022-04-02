cd98ff643325c863fd08409da77668e6fbe3605b
Serhii Hromnadzkyi <serge.alex.gr@gmail.com>
2022-04-02 16:03:48 +0200

    adresses template implemented

--- wp-content/themes/baby-brand/woocommerce/myaccount/my-address.php
+++ wp-content/themes/baby-brand/woocommerce/myaccount/my-address.php
@@ -42,10 +42,6 @@ $oldcol = 1;
 $col    = 1;
 ?>
 
-<p>
-	<?php echo apply_filters( 'woocommerce_my_account_my_address_description', esc_html__( 'The following addresses will be used on the checkout page by default.', 'woocommerce' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-</p>
-
 <?php if ( ! wc_ship_to_billing_address_only() && wc_shipping_enabled() ) : ?>
 	<div class="u-columns woocommerce-Addresses col2-set addresses">
 <?php endif; ?>
@@ -53,20 +49,34 @@ $col    = 1;
 <?php foreach ( $get_addresses as $name => $address_title ) : ?>
 	<?php
 		$address = wc_get_account_formatted_address( $name );
+		$addressFieldsArray = explode('<br/>', $address);
 		$col     = $col * -1;
 		$oldcol  = $oldcol * -1;
 	?>
 
 	<div class="u-column<?php echo $col < 0 ? 1 : 2; ?> col-<?php echo $oldcol < 0 ? 1 : 2; ?> woocommerce-Address">
-		<header class="woocommerce-Address-title title">
-			<h3><?php echo esc_html( $address_title ); ?></h3>
-			<a href="<?php echo esc_url( wc_get_endpoint_url( 'edit-address', $name ) ); ?>" class="edit"><?php echo $address ? esc_html__( 'Edit', 'woocommerce' ) : esc_html__( 'Add', 'woocommerce' ); ?></a>
-		</header>
-		<address>
-			<?php
-				echo $address ? wp_kses_post( $address ) : esc_html_e( 'You have not set up this type of address yet.', 'woocommerce' );
-			?>
-		</address>
+		<div class="adress-page">
+            <div class="adress-page__container container">
+				<div class="adress-page__body">
+					<div class="adress-page__columns">
+						<div class="adress-page__column item-adress">
+							<header class="woocommerce-Address-title title">
+								<div class="item-adress__name">
+									<?php echo esc_html( $address_title ); ?>
+								</div>
+								<div class="item-adress__edit">
+									<a href="<?php echo esc_url( wc_get_endpoint_url( 'edit-address', $name ) ); ?>"><?php echo $address ? esc_html__( 'Edit', 'woocommerce' ) : esc_html__( 'Add', 'woocommerce' ); ?></a>
+								</div>
+							</header>
+
+							<?php foreach ($addressFieldsArray as $field) : ?>
+								<div class="item-adress__form"><?php echo $field; ?></div>
+							<?php endforeach; ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 
 <?php endforeach; ?>
