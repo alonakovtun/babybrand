299a5b1a4808adcaf279362dbf82d45a097682be
alonakovtun <alonakovtun55@gmail.com>
2022-04-04 11:07:04 +0200

    information page

--- wp-content/themes/baby-brand/template-storieslista.php
+++ wp-content/themes/baby-brand/template-storieslista.php
@@ -3,48 +3,47 @@ get_header();
 ?>
 
 <main class="main">
-
             <div class="categories">
                 <div class="categories__body">
                     <div class="categories__column item-category">
-                        <a href="stories.html"><img class="item-category__img" src="img/categoriesimg1.jpg"
+                        <a href="#"><img class="item-category__img" src="/wp-content/themes/baby-brand/assets/img/categoriesimg1.jpg"
                                 alt=""></a>
                         <div class="item-category__text">
                             <p>0/12 M</p>Explore our baby basics - crafted from the softest possible fabrics
                             in unisex monochromatic colours. No logo, no print, no fuss.
                         </div>
-                        <a href="stories.html" class="categories__name">Lorem ipsum dolor sit amet,
+                        <a href="#" class="categories__name">Lorem ipsum dolor sit amet,
                             consectetur adipiscing elit.v</a>
                     </div>
                     <div class="categories__column item-category">
-                        <a href="stories.html"><img class="item-category__img" src="img/categoriesimg3.jpg"
+                        <a href="#"><img class="item-category__img" src="/wp-content/themes/baby-brand/assets/img/categoriesimg3.jpg"
                                 alt=""></a>
                         <div class="item-category__text">
                             <p>0/12 M</p>Explore our baby basics - crafted from the softest possible fabrics
                             in unisex monochromatic colours. No logo, no print, no fuss.
                         </div>
-                        <a href="stories.html" class="categories__name">Lorem ipsum dolor sit amet,
+                        <a href="#" class="categories__name">Lorem ipsum dolor sit amet,
                             consectetur adipiscing elit.v</a>
                     </div>
                     <div class="categories__column item-category">
-                        <a href="stories.html"><img class="item-category__img" src="img/categoriesimg2.jpg"
+                        <a href="#"><img class="item-category__img" src="/wp-content/themes/baby-brand/assets/img/categoriesimg2.jpg"
                                 alt=""></a>
                         <div class="item-category__text">
                             <p>0/12 M</p>Explore our baby basics - crafted from the softest possible
                             fabrics
                             in unisex monochromatic colours. No logo, no print, no fuss.
                         </div>
-                        <a href="stories.html" class="categories__name">Lorem ipsum dolor sit amet,
+                        <a href="#" class="categories__name">Lorem ipsum dolor sit amet,
                             consectetur adipiscing elit.v</a>
                     </div>
                     <div class="categories__column item-category">
-                        <a href="stories.html"><img class="item-category__img" src="img/categoriesimg4.jpg"
+                        <a href="#"><img class="item-category__img" src="/wp-content/themes/baby-brand/assets/img/categoriesimg4.jpg"
                                 alt=""></a>
                         <div class="item-category__text">
                             <p>0/12 M</p> Explore our baby basics - crafted from the softest possible fabrics
                             in unisex monochromatic colours. No logo, no print, no fuss.
                         </div>
-                        <a href="stories.html" class="categories__name">Lorem ipsum dolor sit amet,
+                        <a href="#" class="categories__name">Lorem ipsum dolor sit amet,
                             consectetur adipiscing elit.v</a>
                     </div>
                 </div>
