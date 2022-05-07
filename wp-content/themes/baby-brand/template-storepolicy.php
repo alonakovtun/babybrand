49fb7fc88529fccc767c1025f27ba7d3bb2d1825
alonakovtun <alonakovtun55@gmail.com>
2022-05-07 23:11:26 +0200

    store policy fixed

--- wp-content/themes/baby-brand/template-storepolicy.php
+++ wp-content/themes/baby-brand/template-storepolicy.php
@@ -7,72 +7,72 @@ get_header();
                 <div class="policy__container container link">
                     <div class="policy__body">
                         <div class="policy__column">
-                            <h4 class="policy__title"><?php the_field('caption1') ?></h4>
+                            <p class="policy__title"><?php the_field('caption1') ?></p>
                             <div class="policy__text">
                                 <?php the_field('content1') ?>
                             </div>
                         </div>
                         <div class="policy__column">
-                            <h4 class="policy__title"><?php the_field('caption2') ?></h4>
+                            <p class="policy__title"><?php the_field('caption2') ?></p>
                             <div class="policy__text">
                                 <?php the_field('content2') ?>
                             </div>
                         </div>
                         <div class="policy__column">
-                            <h4 class="policy__title"><?php the_field('caption3') ?></h4>
+                            <p class="policy__title"><?php the_field('caption3') ?></p>
                             <div class="policy__text">
                                 <?php the_field('content3') ?>
                             </div>
                         </div>
                         <div class="policy__column">
-                            <h4 class="policy__title"><?php the_field('caption4') ?></h4>
+                            <p class="policy__title"><?php the_field('caption4') ?></p>
                             <div class="policy__text">
                                 <?php the_field('content4') ?>
                             </div>
                         </div>
                         <div class="policy__column">
-                            <h4 class="policy__title"><?php the_field('caption5') ?></h4>
+                            <p class="policy__title"><?php the_field('caption5') ?></p>
                             <div class="policy__text">
                                 <?php the_field('content5') ?>
                             </div>
                         </div>
                         <div class="policy__column">
-                            <h4 class="policy__title"><?php the_field('caption6') ?></h4>
+                            <p class="policy__title"><?php the_field('caption6') ?></p>
                             <div class="policy__text">
                                 <?php the_field('content6') ?>
                             </div>
                         </div>
                         <div class="policy__column">
-                            <h4 class="policy__title"><?php the_field('caption7') ?></h4>
+                            <p class="policy__title"><?php the_field('caption7') ?></p>
                             <div class="policy__text">
                                 <?php the_field('content7') ?>
                             </div>
                         </div>
                         <div class="policy__column">
-                            <h4 class="policy__title"><?php the_field('caption8') ?></h4>
+                            <p class="policy__title"><?php the_field('caption8') ?></p>
                             <div class="policy__text">
                                 <?php the_field('content8') ?>
                             </div>
                         <div class="policy__column">
-                            <h4 class="policy__title"><?php the_field('caption9') ?></h4>
+                            <p class="policy__title"><?php the_field('caption9') ?></p>
                             <div class="policy__text">
                                 <?php the_field('content9') ?>
                             </div>
                         </div>
                         <div class="policy__column">
-                            <h4 class="policy__title"><?php the_field('caption10') ?></h4>
+                            <p class="policy__title"><?php the_field('caption10') ?></p>
                             <div class="policy__text">
                                 <?php the_field('content10') ?>
                             </div>
                         </div>
                         <div class="policy__column">
-                            <h4 class="policy__title"><?php the_field('caption11') ?></h4>
+                            <p class="policy__title"><?php the_field('caption11') ?></p>
                             <div class="policy__text">
                                 <?php the_field('content11') ?>
                             </div>
                         </div>
                         <div class="policy__column">
-                            <h4 class="policy__title"><?php the_field('caption12') ?></h4>
+                            <p class="policy__title"><?php the_field('caption12') ?></p>
                             <div class="policy__text">
                                 <?php the_field('content12') ?>
                             </div>
