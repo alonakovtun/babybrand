5d7775677779246a79876015c5b04a50eba01c77
alonakovtun <alonakovtun55@gmail.com>
2022-05-09 21:40:33 +0200

    mobile wishlist, reset password

--- wp-content/plugins/yith-woocommerce-wishlist/templates/wishlist-view-mobile.php
+++ wp-content/plugins/yith-woocommerce-wishlist/templates/wishlist-view-mobile.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * Wishlist page template - Standard Layout
  *
  * @author YITH
  * @package YITH\Wishlist\Templates\Wishlist\View
- * @version 3.0.11
+ * @version 3.0.0
  */
 
 /**
@@ -41,222 +42,97 @@
  * @var $no_interactions               bool
  */
 
-if ( ! defined( 'YITH_WCWL' ) ) {
+if (!defined('YITH_WCWL')) {
 	exit;
 } // Exit if accessed directly
 ?>
 
-<!-- WISHLIST MOBILE -->
-<ul
-	class="shop_table cart wishlist_table wishlist_view responsive mobile <?php echo $show_cb ? 'with-checkbox' : ''; ?> <?php echo $no_interactions ? 'no-interactions' : ''; ?>"
-	data-pagination="<?php echo esc_attr( $pagination ); ?>" data-per-page="<?php echo esc_attr( $per_page ); ?>" data-page="<?php echo esc_attr( $current_page ); ?>"
-	data-id="<?php echo esc_attr( $wishlist_id ); ?>" data-token="<?php echo esc_attr( $wishlist_token ); ?>">
+<main class="main">
+	<div class="wishlist">
+		<div class="wishlist__container">
+			<div class="wishlist__body link mt-65">
+				<?php
+				if ($wishlist && $wishlist->has_items()) :
+					foreach ($wishlist_items as $item) :
+						/**
+						 * Each of the wishlist items
+						 *
+						 * @var $item \YITH_WCWL_Wishlist_Item
+						 */
+						global $product;
 
-	<?php
-	if ( $wishlist && $wishlist->has_items() ) :
-		foreach ( $wishlist_items as $item ) :
-			/**
-			 * Each of wishlist items
-			 *
-			 * @var $item \YITH_WCWL_Wishlist_Item
-			 */
-			global $product;
+						$product      = $item->get_product();
+						$availability = $product->get_availability();
+						$stock_status = isset($availability['class']) ? $availability['class'] : false;
 
-			$product      = $item->get_product();
-			$availability = $product->get_availability();
-			$stock_status = isset( $availability['class'] ) ? $availability['class'] : false;
-
-			if ( $product && $product->exists() ) :
+						if ($product && $product->exists()) :
 				?>
-				<li id="yith-wcwl-row-<?php echo esc_attr( $item->get_product_id() ); ?>" data-row-id="<?php echo esc_attr( $item->get_product_id() ); ?>">
-					<?php if ( $show_cb ) : ?>
-						<div class="product-checkbox">
-							<input type="checkbox" value="yes" name="items[<?php echo esc_attr( $item->get_product_id() ); ?>][cb]"/>
-						</div>
-					<?php endif ?>
+							<div class="wishlist__column item-card">
+								<div class="wishlist__img">
+									<?php do_action('yith_wcwl_table_before_product_thumbnail', $item, $wishlist); ?>
 
-					<div class="item-wrapper">
-						<div class="product-thumbnail">
-							<a href="<?php echo esc_url( get_permalink( apply_filters( 'woocommerce_in_cart_product', $item->get_product_id() ) ) ); ?>">
-								<?php echo wp_kses_post( $product->get_image() ); ?>
-							</a>
-						</div>
-
-						<div class="item-details">
-							<div class="product-name">
-								<h3>
-									<a href="<?php echo esc_url( get_permalink( apply_filters( 'woocommerce_in_cart_product', $item->get_product_id() ) ) ); ?>">
-										<?php echo wp_kses_post( apply_filters( 'woocommerce_in_cartproduct_obj_title', $product->get_title(), $product ) ); ?>
+									<a href="<?php echo esc_url(get_permalink(apply_filters('woocommerce_in_cart_product', $item->get_product_id()))); ?>">
+										<?php echo wp_kses_post($product->get_image()); ?>
 									</a>
-								</h3>
-								<?php do_action( 'yith_wcwl_table_after_product_name', $item ); ?>
-							</div>
 
-							<?php if ( $show_variation || $show_dateadded || $show_price ) : ?>
-								<table class="item-details-table">
+									<?php do_action('yith_wcwl_table_after_product_thumbnail', $item, $wishlist); ?>
+								</div>
+								<div class="wishlist-body card-body">
+									<div class="wishlist__row">
+										<div class="wishlist-body__name">
+											<?php do_action('yith_wcwl_table_before_product_name', $item, $wishlist); ?>
 
-									<?php if ( $show_variation && $product->is_type( 'variation' ) ) : ?>
-										<?php
-										/**
-										 * Product object representing variation for current item
-										 *
-										 * @var $product \WC_Product_Variation
-										 */
-										$attributes = $product->get_attributes();
+											<a href="<?php echo esc_url(get_permalink(apply_filters('woocommerce_in_cart_product', $item->get_product_id()))); ?>">
+												<?php echo wp_kses_post(apply_filters('woocommerce_in_cartproduct_obj_title', $product->get_title(), $product)); ?>
+											</a>
 
-										if ( ! empty( $attributes ) ) :
-											foreach ( $attributes as $name => $value ) :
-												if ( ! taxonomy_exists( $name ) ) {
-													continue;
-												}
-
-												$attribute = get_term_by( 'slug', $value, $name );
-
-												if ( ! is_wp_error( $attribute ) && ! empty( $attribute->name ) ) {
-													$value = $attribute->name;
-												}
-												?>
-												<tr>
-													<td class="label">
-														<?php echo esc_attr( wc_attribute_label( $name, $product ) ); ?>:
-													</td>
-													<td class="value">
-														<?php echo esc_attr( rawurldecode( $value ) ); ?>
-													</td>
-												</tr>
-												<?php
-											endforeach;
-										endif;
-										?>
-									<?php endif; ?>
-
-									<?php if ( $show_dateadded && $item->get_date_added() ) : ?>
-										<tr>
-											<td class="label">
-												<?php esc_html_e( 'Added on:', 'yith-woocommerce-wishlist' ); ?>
-											</td>
-											<td class="value">
-												<?php echo esc_html( $item->get_date_added_formatted() ); ?>
-											</td>
-										</tr>
-									<?php endif; ?>
-
-									<?php if ( $show_price || $show_price_variations ) : ?>
-										<tr>
-											<td class="label">
-												<?php esc_html_e( 'Price:', 'yith-woocommerce-wishlist' ); ?>
-											</td>
-											<td class="value">
-												<?php
-												if ( $show_price ) {
-													echo wp_kses_post( $item->get_formatted_product_price() );
-												}
-
-												if ( $show_price_variations ) {
-													echo wp_kses_post( $item->get_price_variation() );
-												}
-												?>
-											</td>
-										</tr>
-									<?php endif ?>
-
-								</table>
-							<?php endif; ?>
-						</div>
-					</div>
-
-					<div class="additional-info-wrapper">
-						<?php if ( $show_quantity || $show_stock_status ) : ?>
-							<table class="additional-info">
-								<?php if ( $show_quantity ) : ?>
-									<tr>
-										<td class="label">
-											<?php esc_html_e( 'Quantity:', 'yith-woocommerce-wishlist' ); ?>
-										</td>
-										<td class="value">
-											<?php if ( ! $no_interactions && $wishlist->current_user_can( 'update_quantity' ) ) : ?>
-												<input type="number" min="1" step="1" name="items[<?php echo esc_attr( $item->get_product_id() ); ?>][quantity]" value="<?php echo esc_attr( $item->get_quantity() ); ?>"/>
-											<?php else : ?>
-												<?php echo esc_html( $item->get_quantity() ); ?>
-											<?php endif; ?>
-										</td>
-									</tr>
-								<?php endif; ?>
-
-								<?php if ( $show_stock_status ) : ?>
-									<tr>
-										<td class="label">
-											<?php esc_html_e( 'Stock:', 'yith-woocommerce-wishlist' ); ?>
-										</td>
-										<td class="value">
-											<?php echo 'out-of-stock' === $stock_status ? '<span class="wishlist-out-of-stock">' . esc_html__( 'Out of stock', 'yith-woocommerce-wishlist' ) . '</span>' : '<span class="wishlist-in-stock">' . esc_html__( 'In Stock', 'yith-woocommerce-wishlist' ) . '</span>'; ?>
-										</td>
-									</tr>
-								<?php endif ?>
-							</table>
-						<?php endif; ?>
-
-						<!-- Add to cart button -->
-						<?php $show_add_to_cart = apply_filters( 'yith_wcwl_table_product_show_add_to_cart', $show_add_to_cart, $item, $wishlist ); ?>
-						<?php if ( $show_add_to_cart && isset( $stock_status ) && 'out-of-stock' !== $stock_status ) : ?>
-							<div class="product-add-to-cart">
-								<?php woocommerce_template_loop_add_to_cart( array( 'quantity' => $show_quantity ? $item->get_quantity() : 1 ) ); ?>
-							</div>
-						<?php endif ?>
-
-						<!-- Change wishlist -->
-						<?php if ( $move_to_another_wishlist && $available_multi_wishlist && count( $users_wishlists ) > 1 ) : ?>
-							<div class="move-to-another-wishlist">
-								<?php if ( 'select' === $move_to_another_wishlist_type ) : ?>
-									<select class="change-wishlist selectBox">
-										<option value=""><?php esc_html_e( 'Move', 'yith-woocommerce-wishlist' ); ?></option>
-										<?php
-										foreach ( $users_wishlists as $wl ) :
-											/**
-											 * Each of customer wishlists
-											 *
-											 * @var $wl \YITH_WCWL_Wishlist
-											 */
-											if ( $wl->get_token() === $wishlist_token ) {
-												continue;
+											<?php
+											if ($show_variation && $product->is_type('variation')) {
+												/**
+												 * Product is a Variation
+												 *
+												 * @var $product \WC_Product_Variation
+												 */
+												echo wp_kses_post(wc_get_formatted_variation($product));
 											}
 											?>
-											<option value="<?php echo esc_attr( $wl->get_token() ); ?>">
-												<?php echo esc_html( sprintf( '%s - %s', $wl->get_formatted_name(), $wl->get_formatted_privacy() ) ); ?>
-											</option>
-											<?php
-										endforeach;
-										?>
-									</select>
-								<?php else : ?>
-									<a href="#move_to_another_wishlist" class="move-to-another-wishlist-button" data-rel="prettyPhoto[move_to_another_wishlist]">
-										<?php echo esc_html( apply_filters( 'yith_wcwl_move_to_another_list_label', __( 'Move to another list &rsaquo;', 'yith-woocommerce-wishlist' ) ) ); ?>
-									</a>
-								<?php endif; ?>
-							</div>
-						<?php endif; ?>
 
-						<?php if ( $show_remove_product || $repeat_remove_button ) : ?>
-							<div class="product-remove">
-								<a href="<?php echo esc_url( $item->get_remove_url() ); ?>" class="remove_from_wishlist" title="<?php echo esc_html( apply_filters( 'yith_wcwl_remove_product_wishlist_message_title', __( 'Remove this product', 'yith-woocommerce-wishlist' ) ) ); ?>"><i class="fa fa-trash"></i></a>
+											<?php do_action('yith_wcwl_table_after_product_name', $item, $wishlist); ?>
+										</div>
+										<div class="card-body__price">
+											<?php if ($show_price || $show_price_variations) : ?>
+												<?php do_action('yith_wcwl_table_before_product_price', $item, $wishlist); ?>
+
+												<?php
+												if ($show_price) {
+													echo wp_kses_post($item->get_formatted_product_price());
+												}
+
+												if ($show_price_variations) {
+													echo wp_kses_post($item->get_price_variation());
+												}
+												?>
+
+												<?php do_action('yith_wcwl_table_after_product_price', $item, $wishlist); ?>
+
+											<?php endif ?>
+										</div>
+									</div>
+									<div class="wishlist-body__remove">
+										<?php if ($repeat_remove_button) : ?>
+											<a href="<?php echo esc_url($item->get_remove_url()); ?>" class="remove_from_wishlist button" title="<?php echo esc_html(apply_filters('yith_wcwl_remove_product_wishlist_message_title', __('Remove this product', 'yith-woocommerce-wishlist'))); ?>">
+												<?php esc_html_e('Remove', 'yith-woocommerce-wishlist'); ?>
+											</a>
+										<?php endif; ?>
+									</div>
+								</div>
 							</div>
-						<?php endif; ?>
-					</div>
-				</li>
 				<?php
-			endif;
-		endforeach;
-	else :
-		?>
-		<p class="wishlist-empty">
-			<?php echo esc_html( apply_filters( 'yith_wcwl_no_product_to_remove_message', __( 'No products added to the wishlist', 'yith-woocommerce-wishlist' ) ) ); ?>
-		</p>
-	<?php endif; ?>
-
-</ul>
-
-<?php if ( ! empty( $page_links ) ) : ?>
-	<nav class="wishlist-pagination">
-		<?php echo wp_kses_post( $page_links ); ?>
-	</nav>
-<?php endif; ?>
+						endif;
+					endforeach;
+				else : echo esc_html(apply_filters('yith_wcwl_no_product_to_remove_message', __('No products added to the wishlist', 'yith-woocommerce-wishlist'), $wishlist));
+				endif ?>
+			</div>
+		</div>
+	</div>
+</main>
--- wp-content/themes/baby-brand/woocommerce/myaccount/form-reset-password.php
+++ wp-content/themes/baby-brand/woocommerce/myaccount/form-reset-password.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Lost password reset form.
  *
@@ -15,39 +16,44 @@
  * @version 3.5.5
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-do_action( 'woocommerce_before_reset_password_form' );
+do_action('woocommerce_before_reset_password_form');
 ?>
+<div class="login__body register lost">
+	<div class="login__column ">
+		<div class="u-column2 col-2">
+			<form method="post" class="item-login__form woocommerce-ResetPassword lost_reset_password">
 
-<form method="post" class="woocommerce-ResetPassword lost_reset_password">
+				<p class="item-login__name"><?php echo apply_filters('woocommerce_reset_password_message', esc_html__('Enter a new password below.', 'woocommerce')); ?></p><?php // @codingStandardsIgnoreLine 
+																																						?>
 
-	<p><?php echo apply_filters( 'woocommerce_reset_password_message', esc_html__( 'Enter a new password below.', 'woocommerce' ) ); ?></p><?php // @codingStandardsIgnoreLine ?>
+				<p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
+					<!-- <label for="password_1"><?php esc_html_e('New password', 'woocommerce'); ?>&nbsp;<span class="required">*</span></label> -->
+					<input placeholder="<?php esc_html_e('New password', 'woocommerce'); ?>" type="password" class="woocommerce-Input woocommerce-Input--text input-text item-login__pass" name="password_1" id="password_1" autocomplete="new-password" />
+				</p>
+				<p class="woocommerce-form-row woocommerce-form-row--last form-row form-row-last">
+					<!-- <label for="password_2"><?php esc_html_e('Re-enter new password', 'woocommerce'); ?>&nbsp;<span class="required">*</span></label> -->
+					<input placeholder="<?php esc_html_e('Re-enter new password', 'woocommerce'); ?>" type="password" class="woocommerce-Input woocommerce-Input--text input-text  item-login__pass" name="password_2" id="password_2" autocomplete="new-password" />
+				</p>
 
-	<p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
-		<label for="password_1"><?php esc_html_e( 'New password', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-		<input type="password" class="woocommerce-Input woocommerce-Input--text input-text" name="password_1" id="password_1" autocomplete="new-password" />
-	</p>
-	<p class="woocommerce-form-row woocommerce-form-row--last form-row form-row-last">
-		<label for="password_2"><?php esc_html_e( 'Re-enter new password', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-		<input type="password" class="woocommerce-Input woocommerce-Input--text input-text" name="password_2" id="password_2" autocomplete="new-password" />
-	</p>
+				<input type="hidden" name="reset_key" value="<?php echo esc_attr($args['key']); ?>" />
+				<input type="hidden" name="reset_login" value="<?php echo esc_attr($args['login']); ?>" />
 
-	<input type="hidden" name="reset_key" value="<?php echo esc_attr( $args['key'] ); ?>" />
-	<input type="hidden" name="reset_login" value="<?php echo esc_attr( $args['login'] ); ?>" />
+				<div class="clear"></div>
 
-	<div class="clear"></div>
+				<?php do_action('woocommerce_resetpassword_form'); ?>
 
-	<?php do_action( 'woocommerce_resetpassword_form' ); ?>
+				<p class="woocommerce-form-row form-row">
+					<input type="hidden" name="wc_reset_password" value="true" />
+					<button type="submit" class="item-login__btn woocommerce-Button button" value="<?php esc_attr_e('Save', 'woocommerce'); ?>"><?php esc_html_e('Save', 'woocommerce'); ?></button>
+				</p>
 
-	<p class="woocommerce-form-row form-row">
-		<input type="hidden" name="wc_reset_password" value="true" />
-		<button type="submit" class="woocommerce-Button button" value="<?php esc_attr_e( 'Save', 'woocommerce' ); ?>"><?php esc_html_e( 'Save', 'woocommerce' ); ?></button>
-	</p>
+				<?php wp_nonce_field('reset_password', 'woocommerce-reset-password-nonce'); ?>
 
-	<?php wp_nonce_field( 'reset_password', 'woocommerce-reset-password-nonce' ); ?>
-
-</form>
+			</form>
+		</div>
+	</div>
+</div>
 <?php
-do_action( 'woocommerce_after_reset_password_form' );
-
+do_action('woocommerce_after_reset_password_form');
